67ad15c5a270eb81bb9b4df5b5378eec1981a174
Joshue2806 <joshue.ruiz@est.ikiam.edu.ec>
2021-12-11 18:23:52 -0500

    Agregado el archivo con el que se trabaja

--- tarea1/netsize.sh
+++ tarea1/netsize.sh
@@ -1,4 +1,5 @@
-cd D:/Bioinformática/CSB-master/unix/data/Saavedra2013
-echo "Número de filas:"  
-wc -l < n8.txt  
-echo "Número de columnas:"
+ cd D:/Bioinformática/CSB-master/unix/data/Saavedra2013 #Aquí llamamos al archivo de su ubicación}
+
+ echo "Archivo seleccionado: n8.txt" #echo imprime el texto que queremos que se mencione antes del resultado
+ echo "Número de filas:"
+ wc -l < n8.txt   #En este contamos el número de filas o líneas con wc -l y < es para que no se vuelva a imprimir el nombre del archivo
