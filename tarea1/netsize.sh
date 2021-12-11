fe9cdb8143a8726720bb444f8ab533f79b44ebf7
Joshue2806 <joshue.ruiz@est.ikiam.edu.ec>
2021-12-11 18:46:11 -0500

    Cambio de nota en la primera línea, e inicio del conteo de columnas

--- tarea1/netsize.sh
+++ tarea1/netsize.sh
@@ -1,5 +1,8 @@
- cd D:/Bioinformática/CSB-master/unix/data/Saavedra2013 #Aquí llamamos al archivo de su ubicación}
+cd D:/Bioinformática/CSB-master/unix/data/Saavedra2013 #Aquí cd nos lleva al lugar donde se encuentra el archivo
 
- echo "Archivo seleccionado: n8.txt" #echo imprime el texto que queremos que se mencione antes del resultado
- echo "Número de filas:"
- wc -l < n8.txt   #En este contamos el número de filas o líneas con wc -l y < es para que no se vuelva a imprimir el nombre del archivo
+echo "Archivo seleccionado: n8.txt" #echo imprime el texto que queremos que se mencione antes del resultado
+echo "Número de filas:"
+wc -l < n8.txt   #En este contamos el número de filas o líneas con wc -l y < es para que no se vuelva a imprimir el nombre del archivo
+
+echo "Número de columnas:"
+head -n 1 n8.txt 
