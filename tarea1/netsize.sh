0eefc3130e0991deb0dd8bed7b33b4178f0cf4b1
Joshue2806 <joshue.ruiz@est.ikiam.edu.ec>
2021-12-11 19:05:32 -0500

    Añadido de "|" en el conteo de columnas para que funcione

--- tarea1/netsize.sh
+++ tarea1/netsize.sh
@@ -5,6 +5,6 @@ echo "Número de filas:"
 wc -l < n8.txt   #En este contamos el número de filas o líneas con wc -l y < es para que no se vuelva a imprimir el nombre del archivo
 
 echo "Número de columnas:"
-head -n 1 n8.txt | tr -d " " | tr -d "\n" wc -c #Aquí para contar las columnas tomamos la primera fila, reducimos los espacios entre columnas, además como se menciona en el ejercicio igualmente eliminamos el espacio al final representado por "\n" y contamos los caracteres con wc -c que nos da el número de columnas
+head -n 1 n8.txt | tr -d " " | tr -d "\n" | wc -c #Aquí para contar las columnas tomamos la primera fila, reducimos los espacios entre columnas, además como se menciona en el ejercicio igualmente eliminamos el espacio al final representado por "\n" y contamos los caracteres con wc -c que nos da el número de columnas
 
 #Si se desea correr este código, realizarlo en Git Bash
