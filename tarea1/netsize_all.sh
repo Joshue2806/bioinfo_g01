2ed59dbad664854dfb0d31f8bc5630c87ba1e54c
Joshue2806 <joshue.ruiz@est.ikiam.edu.ec>
2021-12-11 20:13:45 -0500

    Aquí ya escribimos parte del comando realizado

--- tarea1/netsize_all.sh
+++ tarea1/netsize_all.sh
@@ -1,1 +1,12 @@
-cd D:/Bioinformática/CSB-master/unix/data/Saavedra2013 #Aquí igualmente cd nos lleva al lugar donde se encuentran los archivo
+cd D:/Bioinformática/CSB-master/unix/data/Saavedra2013 #Aquí igualmente cd nos lleva al lugar donde se encuentran los archivos
+
+echo "Número de filas y columnas:"
+
+#Para este ejercicio necesitamos utilizar un bucle con el comando for
+
+for file in *.txt; #De esta manera llamamos a todos los archivos .txt de la carpeta Saavedra2013, "file" es la manera en como los llamaremos a todos
+do #Empieza el bucle, aquí llamamos a dos comandos que sucederan en el bucle para filas y columnas
+filas= wc -l $file; #Este comando es el de filas, se aplica sobre todos los archivos .txt de Saavedra2013 ($file)
+colum= head -n 1 $file | tr -d " " | tr -d "\n" | wc -c;
+echo $file $filas $colum;
+done
