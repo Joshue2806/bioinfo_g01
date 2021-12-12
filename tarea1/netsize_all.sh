8ebe18ab0105737dc802356a19b4af7c18376bd3
Joshue2806 <joshue.ruiz@est.ikiam.edu.ec>
2021-12-11 21:00:46 -0500

    Prueba para mejora de visualización

--- tarea1/netsize_all.sh
+++ tarea1/netsize_all.sh
@@ -5,10 +5,8 @@ cd D:/Bioinformática/CSB-master/unix/data/Saavedra2013 #Aquí igualmente cd nos
 for file in *.txt; #De esta manera llamamos a todos los archivos .txt de la carpeta Saavedra2013, "file" es la manera en como los llamaremos a todos
 do #Empieza el bucle, aquí llamamos a dos comandos que sucederan en el bucle para filas y columnas
 echo $file | sort; #llamaremos a todos los archivos y se los ordenará con sort
-echo "Filas:";
-wc -l < $file; #Este comando es el de filas, se aplica sobre todos los archivos .txt de Saavedra2013 ($file)
-echo "Columnas:";
-head -n 1 $file | tr -d " " | tr -d "\n" | wc -c; #Igual que en la línea 11, se aplica el comando para contar columnas
+echo "Filas:" `wc -l < $file`; #Este comando es el de filas, se aplica sobre todos los archivos .txt de Saavedra2013 ($file)
+echo "Columnas:" `head -n 1 $file | tr -d " " | tr -d "\n" | wc -c`; #Igual que en la línea 11, se aplica el comando para contar columnas
 done #Se acaba el bucle
 
 #Si se desea correr este código, realizarlo en Git Bash con el comando "bash netsize_all.sh"
