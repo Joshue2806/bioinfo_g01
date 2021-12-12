2cdd4116745b7a6bf1058169f27908473bd85102
Joshue2806 <joshue.ruiz@est.ikiam.edu.ec>
2021-12-11 19:02:54 -0500

    Agregado de comando en el conteo de columnas para solo contar las columnas con datos

--- tarea1/netsize.sh
+++ tarea1/netsize.sh
@@ -5,4 +5,4 @@ echo "Número de filas:"
 wc -l < n8.txt   #En este contamos el número de filas o líneas con wc -l y < es para que no se vuelva a imprimir el nombre del archivo
 
 echo "Número de columnas:"
-head -n 1 n8.txt | tr -d " " | wc -c #Aquí para contar las columnas tomamos la primera fila y reducimos los espacio entre columnas y contamos los caracteres con wc -c que nos da el número de columnas
+head -n 1 n8.txt | tr -d " " | tr -d "\n" wc -c #Aquí para contar las columnas tomamos la primera fila, reducimos los espacios entre columnas, eliminamos el espacio al final representado por "\n" y contamos los caracteres con wc -c que nos da el número de columnas
