cde4914d7d0e7aa5eb862831fa398ed03fa8c7a7
Joshue2806 <joshue.ruiz@est.ikiam.edu.ec>
2021-12-11 20:39:28 -0500

    Mejora del comando para una mejor visualización de las respuestas

--- tarea1/netsize_all.sh
+++ tarea1/netsize_all.sh
@@ -6,7 +6,9 @@ echo "Número de filas y columnas:"
 
 for file in *.txt; #De esta manera llamamos a todos los archivos .txt de la carpeta Saavedra2013, "file" es la manera en como los llamaremos a todos
 do #Empieza el bucle, aquí llamamos a dos comandos que sucederan en el bucle para filas y columnas
-filas= wc -l $file; #Este comando es el de filas, se aplica sobre todos los archivos .txt de Saavedra2013 ($file)
-colum= head -n 1 $file | tr -d " " | tr -d "\n" | wc -c; #Igual que en la línea de arriba, se aplica el comando para contar columnas
-echo $file $filas $colum;
-done
+echo $file | sort; #llamaremos a todos los archivos y se los ordenará con sort
+echo "Filas:";
+wc -l $file; #Este comando es el de filas, se aplica sobre todos los archivos .txt de Saavedra2013 ($file)
+echo "Columnas:";
+head -n 1 $file | tr -d " " | tr -d "\n" | wc -c; #Igual que en la línea 11, se aplica el comando para contar columnas
+done #Se acaba el bucle
